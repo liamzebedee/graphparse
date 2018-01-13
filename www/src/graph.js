216e606e86f528071015cb1100b2d340d14ffd20
Liam Edwards-Playne <liamz@Help-Im-stuck-inside-a-speaker-I-dont-have-much-ti.local>
2018-01-13 13:51:48 +1000

    added search

--- www/src/graph.js
+++ www/src/graph.js
@@ -5,8 +5,10 @@ import './graph.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { observable, computed } from 'mobx';
+import mobx, { observable, computed } from 'mobx';
 import { observer } from "mobx-react";
+import Fuse from 'fuse.js';
+import _ from 'underscore';
 
 // Cheers https://bl.ocks.org/puzzler10/4438752bb93f45dc5ad5214efaa12e4a
 
@@ -16,13 +18,20 @@ import { observer } from "mobx-react";
 // Use donut chart for import visualisation https://bl.ocks.org/mbostock/3887193
 // Use circular segment for contribution viz https://bl.ocks.org/mbostock/3422480
 
+// Zoomable sub circles https://bl.ocks.org/mbostock/7607535
+
 class GraphStore {
-  @observable selectionId = null;
   @observable hoverId = null;
+  @observable highlightedNodes = null;
+  @observable selectedNodeIds = [];
 
   @computed get selectionInfo() {
     let key = this.hoverId || this.selectionId;
     return graphdata.nodesLookup[key]
+  }
+
+  @computed.struct get selectedNodes() {
+    return Array.from(this.selectedNodeIds).map(id => { console.log(id); return graphdata.nodesLookup[id] })
   }
 
   mouseoverNode(id) {
@@ -33,32 +42,109 @@ class GraphStore {
     this.hoverId = null;
   }
 
-  toggleSelection(id) {
-    if(this.selectionId) this.selectionId = null;
-    else this.selectionId = id;
+  selectNode(id) {
+    if(!_.contains(this.selectedNodeIds, id))
+      this.selectedNodeIds.push(id)
   }
+  // toggleSelection(id) {
+    // if(this.selectionId) this.selectionId = null;
+    // else this.selectionId = id;
+  // }
 
   clearSelection(id) {
     this.selectionId = null;
+  }
+
+  searchAndHighlightNodes(query) {
+    if(query == "") {
+      this.highlightedNodes = null;
+      return
+    }
+    let matches = [];
+
+    function splitId(id) {
+      // return id.split(/(?=[A-Z])/g).reverse();
+      return [id]
+    }
+
+    let searchData = graphdata.nodes.map(node => {
+      return {
+        id: node.id, 
+        label: splitId(node.label),
+      }
+    })
+
+    var options = {
+      shouldSort: true,
+      findAllMatches: true,
+      includeScore: true,
+      threshold: 0.3,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 2,
+      keys: [
+        "label"
+      ]
+    };
+
+    let fuse = new Fuse(searchData, options);
+    let results = fuse.search(query)
+    this.highlightedNodes = results.filter(el => el.score < 0.7)
+  }
+
+  getNodeHighlight(id) {
+    return this.highlightedNodes.filter(el => el.item.id == id)[0];
   }
 }
 
 const graphStore = new GraphStore()
 
+const LoadingWrap = (props) => {
+  if(props.data != null) return props.children;
+  else return "";
+}
+
 @observer
 class InfoView extends React.Component {
+  searchNodes = (ev) => {
+    this.props.store.searchAndHighlightNodes(ev.target.value)
+  }
+
   render() {
     const store = this.props.store;
-    return store.selectionInfo ? <SelectionInfo sel={store.selectionInfo}/> : null;
+
+    let selectedNodes = store.selectedNodes;
+
+    return <div>
+      <input type="text" placeholder="&#x1F50E; Find structs, funcs, methods..." className='search' onChange={this.searchNodes}/>
+      <SelectedNodes selectedNodes={selectedNodes}/>
+      <LoadingWrap data={store.selectionInfo}><SelectionInfo sel={store.selectionInfo}/></LoadingWrap>
+    </div>
   }
 }
 
-const SelectionInfo = (props) => {
+const SelectedNodes = (props) => {
   return <div>
+    {props.selectedNodes.map((node, i) => <SelectedNode key={i} node={node}/>)}
+  </div>
+}
+
+const SelectedNode = (props) => {
+  return <div>
+    <div style={{ backgroundColor: nodeColor(props.node.variant) }}>{props.node.label}</div>
+  </div>
+}
+
+const SelectionInfo = (props) => {
+  return <div style={{ padding:'1em' }}>
     {graphdata.nodeTypes[props.sel.variant]} <br/>
     <b>{props.sel.label}</b>
   </div>
 }
+
+
+var nodeColor = d3.scaleOrdinal(d3.schemeCategory20);
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
@@ -85,7 +171,6 @@ document.addEventListener('DOMContentLoaded', function() {
   .append("svg:path")
     .attr("d", "M0,-5L10,0L0,5");
 
-  var nodeColor = d3.scaleOrdinal(d3.schemeCategory20);
   
 
   // Setup simulation
@@ -95,12 +180,11 @@ document.addEventListener('DOMContentLoaded', function() {
                                 
   var link_force =  d3.forceLink(graphdata.edges)
                       .id(function(d) { return d.id; })
-                      .strength(2)
+                      .strength(1)
                       .distance(10)
   
   var charge_force = d3.forceManyBody()
-                      //  .strength(-200)
-
+                       .strength(-200)
 
   simulation
       .nodes(graphdata.nodes)
@@ -111,7 +195,6 @@ document.addEventListener('DOMContentLoaded', function() {
         return 15 + radius(d)
       }))
    ;
-  
           
   //add tick instructions: 
   simulation.on("tick", tickActions );
@@ -131,7 +214,7 @@ document.addEventListener('DOMContentLoaded', function() {
               .attr("marker-end", "url(#end)");
   
 
-  //draw circles for the nodes 
+  //draw circles for the nodes
   var node = g.append("g")
           .attr("class", "nodes") 
           .selectAll("g")
@@ -145,9 +228,12 @@ document.addEventListener('DOMContentLoaded', function() {
             graphStore.mouseoutNode(d.id)
           })
           .on('click', function(d) {
-            graphStore.toggleSelection(d.id)
+            // graphStore.toggleSelection(d.id)
+            graphStore.selectNode(d.id)
           })
+
   
+
 function radius(d) {
   const cradius = 18; 
   return cradius * d.rank;
@@ -159,6 +245,25 @@ function radius(d) {
               })
               .attr("fill", function(d) { return nodeColor(d.variant); })
   
+  mobx.autorun(() => {
+    if(graphStore.highlightedNodes == null) {
+      node.attr('opacity', 1)
+      link.attr('opacity', 1)
+    } else {
+      node
+      .attr('opacity', 0.2)
+      .filter(function(d) {
+        return graphStore.getNodeHighlight(d.id) != null;
+      })
+      .attr('opacity', function(d) {
+        return 1 - graphStore.getNodeHighlight(d.id).score;
+      });
+
+      link
+      .attr('opacity', 0)
+    }
+  })
+
   let label = node.append('text')
               .text(function(d) { return d.label })
               .style("font-size", function(d) {
@@ -209,44 +314,32 @@ function radius(d) {
   
 
   function tickActions() {
+    link.attr("d", function(d) {
+      // Total difference in x and y from source to target
+      let diffX = d.target.x - d.source.x;
+      let diffY = d.target.y - d.source.y;
 
-  //   link.attr("d", function(d) {
-  //     var dx = d.target.x - d.source.x,
-  //         dy = d.target.y - d.source.y,
-  //         dr = Math.sqrt(dx * dx + dy * dy);
-  //     return "M" + 
-  //         d.source.x + "," + 
-  //         d.source.y + "A" + 
-  //         dr + "," + dr + " 0 0,1 " + 
-  //         d.target.x + "," + 
-  //         d.target.y;
-  // });
-  link.attr("d", function(d) {
-    // Total difference in x and y from source to target
-    let diffX = d.target.x - d.source.x;
-    let diffY = d.target.y - d.source.y;
+      // Length of path from center of source node to center of target node
+      let pathLength = Math.sqrt((diffX * diffX) + (diffY * diffY));
 
-    // Length of path from center of source node to center of target node
-    let pathLength = Math.sqrt((diffX * diffX) + (diffY * diffY));
+      // x and y distances from center to outside edge of target node
 
-    // x and y distances from center to outside edge of target node
+      let offsetX = (diffX * radius(d.target)) / pathLength;
+      let offsetY = (diffY * radius(d.target)) / pathLength;
 
-    let offsetX = (diffX * radius(d.target)) / pathLength;
-    let offsetY = (diffY * radius(d.target)) / pathLength;
-
-    return "M" + d.source.x + "," + d.source.y + "L" + (d.target.x - offsetX) + "," + (d.target.y - offsetY);
-});
+      return "M" + d.source.x + "," + d.source.y + "L" + (d.target.x - offsetX) + "," + (d.target.y - offsetY);
+    });
   
-      // update circle positions each tick of the simulation 
-         node
-          .attr('transform', (d) => `translate(${d.x} ${d.y})`)
+    // update circle positions each tick of the simulation 
+    node
+      .attr('transform', (d) => `translate(${d.x} ${d.y})`)
           
-      // update link positions 
-      link
-          .attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; });
+    // update link positions 
+    link
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
   } 
 
 
